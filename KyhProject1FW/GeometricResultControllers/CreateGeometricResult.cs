ef603caae32f4d64b452dc1139f708bae9414082
Frederick <frederick.wennborg@gmail.com>
2023-01-10 13:31:56 +0100

    #25 feat: added ICreateResult interface

--- KyhProject1FW/GeometricResultControllers/CreateGeometricResult.cs
+++ KyhProject1FW/GeometricResultControllers/CreateGeometricResult.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KyhProject1FW.Interfaces;
 
 namespace KyhProject1FW.ShapeControllers
 {
-    public class CreateGeometricResult
+    public class CreateGeometricResult : ICreateResult
     {
+
+        public void Create()
+        {
+
+
+
+        }
+        
     }
 }
