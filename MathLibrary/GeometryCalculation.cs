acf841a3cb2304b98ed16c1b9ba6172a735d5d8d
Frederick <frederick.wennborg@gmail.com>
2023-01-13 09:20:25 +0100

    #52 feat:added IGeometry interface in ServiceLibrary

--- MathLibrary/GeometryCalculation.cs
+++ MathLibrary/GeometryCalculation.cs
@@ -1,54 +1,51 @@
 ﻿using System.Drawing;
+using ServiceLibrary.Interfaces;
 
 
 namespace ServiceLibrary
 {
-    public class GeometryCalculation
+    public class GeometryCalculation : IGeometry
     {
-        // private IShape _shape;
-
-        private string _shape;
-        private decimal _b;
-        private decimal _h;
-
-
-        public enum shapeEnum
+        public decimal CalculateRectangelPerimiter(decimal b, decimal h)
         {
-            Rectangle,
-            Paralellogram,
-            Triangle,
-            Rhombus
-        }
-
-        public GeometryCalculation(string shape, decimal b, decimal h)
-        {
-            _shape = shape;
-            _b = b;
-            _h = h;
-
-        }
-
-        public decimal CalculatePerimiter()
-        {
-
             var perimiter = 0m;
-
-            if (Enum.TryParse<shapeEnum>(_shape, ignoreCase: true, out var type) && Enum.IsDefined(shapeEnum.Rectangle))
-            {
-
-                perimiter = 2 * _b + 2 * _h;
-
-                return perimiter;
-            }
-
-
             return perimiter;
-
-
-
-
         }
-
+        public decimal CalculateRectangelArea(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateParallelogramPerimiter(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateParallelogramArea(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateTrianglePerimiter(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateTriangleArea(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateRhombusPerimiter(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
+        public decimal CalculateRhombusArea(decimal b, decimal h)
+        {
+            var perimiter = 0m;
+            return perimiter;
+        }
 
     }
 }
---
+++ MathLibrary/Interfaces/IGeometry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.Interfaces
+{
+    public interface IGeometry
+    {
+        public decimal CalculateRectangelPerimiter(decimal b, decimal h);
+        public decimal CalculateRectangelArea(decimal b, decimal h);
+        public decimal CalculateParallelogramPerimiter(decimal b, decimal h);
+        public decimal CalculateParallelogramArea(decimal b, decimal h);
+        public decimal CalculateTrianglePerimiter(decimal b, decimal h);
+        public decimal CalculateTriangleArea(decimal b, decimal h);
+        public decimal CalculateRhombusPerimiter(decimal b, decimal h);
+        public decimal CalculateRhombusArea(decimal b, decimal h);
+
+    }
+}
